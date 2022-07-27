69d135425834d857c5574821344a9f1b92e5cd96
dustinferioli <dustin.ferioli@icloud.com>
2022-07-27 11:42:01 -0400

    setup for heroku

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const morgan = require('morgan');
 const express = require('express');
 const server = express();
